9d23c49498485daa84e79f035e6f0a4426ed8beb
sarib <saribraja1998@gmail.com>
2026-08-05 03:39:20 -0700

    fourtheenth commit

--- Git-course-work/7.Merge Conflicts/react-sample/animated-graphic/src/App.jsx
+++ Git-course-work/7.Merge Conflicts/react-sample/animated-graphic/src/App.jsx
@@ -8,7 +8,7 @@ export default function App() {
       <Spacecraft />
       <div className="caption">
         <span className="caption__eyebrow">ORR&nbsp;·&nbsp;01</span>
-        <span className="caption__title">a quiet orrery Merge Conflict</span>
+        <span className="caption__title">a quiet orrery Conflict Resolved</span>
       </div>
     </main>
   )
